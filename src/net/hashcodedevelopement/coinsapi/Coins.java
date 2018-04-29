b3686ff270d435e92d2b62c2f1d0e9ae8ff4bbf4
ByCerus <zocks1703@web.de>
2018-04-29 13:50:42 +0200

    Bugfixes

--- src/net/hashcodedevelopement/coinsapi/Coins.java
+++ src/net/hashcodedevelopement/coinsapi/Coins.java
@@ -18,48 +18,67 @@ import net.hashcodedevelopement.coinsapi.CoinsAPI;
 public class Coins extends JavaPlugin {
 
 	String prefix = "§3§lC§b§loins§a§lA§b§lPI §8» §7";
-	
+
 	@Override
 	public void onEnable() {
 		System.out.println(" ");
-		System.out.println("   _____      _                    _____ _____ \n  / ____|    (_)             /\\   |  __ \\_   _|\n | |     ___  _ _ __  ___   /  \\  | |__) || |  \n | |    / _ \\| | '_ \\/ __| / /\\ \\ |  ___/ | |  \n | |___| (_) | | | | \\__ \\/ ____ \\| |    _| |_ \n  \\_____\\___/|_|_| |_|___/_/    \\_\\_|   |_____|");
+		System.out.println(
+				"   _____      _                    _____ _____ \n  / ____|    (_)             /\\   |  __ \\_   _|\n | |     ___  _ _ __  ___   /  \\  | |__) || |  \n | |    / _ \\| | '_ \\/ __| / /\\ \\ |  ___/ | |  \n | |___| (_) | | | | \\__ \\/ ____ \\| |    _| |_ \n  \\_____\\___/|_|_| |_|___/_/    \\_\\_|   |_____|");
 		System.out.println(" ");
 		System.out.println("Coded by HashCodeDevelopement | Cerus");
 		System.out.println(" ");
-		
+
 		getCommand("coins").setExecutor(this);
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(sender instanceof Player){
+		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			CoinsAPI coinsAPI = new CoinsAPI();
-			
-			if(args.length == 0){
-				if(coinsAPI.exist(player.getUniqueId())){
-					player.sendMessage(prefix+"Deine Coins: §a"+coinsAPI.getCoins(player.getUniqueId()));
-				} else {coinsAPI.create(player.getUniqueId()); player.performCommand("coins");}
-			} else if(args.length == 3){
-				if(args[0].equalsIgnoreCase("add")){
-					Player r = Bukkit.getPlayer(args[1]);
-					if(r != null){
-						if (Pattern.matches("[a-zA-Z]+", args[2]) == false) {							
-							coinsAPI.addCoins(r.getUniqueId(), Integer.parseInt(args[2]));
+
+			if (args.length == 0) {
+				if (coinsAPI.exist(player.getUniqueId())) {
+					player.sendMessage(prefix + "Coins: §a" + coinsAPI.getCoins(player.getUniqueId()));
+				} else {
+					coinsAPI.create(player.getUniqueId());
+					player.performCommand("coins");
+				}
+			} else if (args.length == 3) {
+				if (player.hasPermission("coins.admin")) {
+					if (args[0].equalsIgnoreCase("add")) {
+						Player r = Bukkit.getPlayer(args[1]);
+						if (r != null) {
+							if (Pattern.matches("[a-zA-Z]+", args[2]) == false) {
+								coinsAPI.addCoins(r.getUniqueId(), Integer.parseInt(args[2]));
+								player.sendMessage(prefix + "§8§l§a§l+ §6§l" + args[2] + "§8§l]");
+							}
+						}
+					} else if (args[0].equalsIgnoreCase("remove")) {
+						Player r = Bukkit.getPlayer(args[1]);
+						if (r != null) {
+							if (Pattern.matches("[a-zA-Z]+", args[2]) == false) {
+								coinsAPI.removeCoins(r.getUniqueId(), Integer.parseInt(args[2]));
+								player.sendMessage(prefix + "§8§l§c§l- §6§l" + args[2] + "§8§l]");
+							}
+						}
+					} else if (args[0].equalsIgnoreCase("set")) {
+						Player r = Bukkit.getPlayer(args[1]);
+						if (r != null) {
+							if (Pattern.matches("[a-zA-Z]+", args[2]) == false) {
+								coinsAPI.setCoins(r.getUniqueId(), Integer.parseInt(args[2]));
+								player.sendMessage(prefix + "§8§l§e§l= §6§l" + args[2] + "§8§l]");
+							}
 						}
 					}
-				} else if(args[0].equalsIgnoreCase("remove")){
+				}
+				if (args[0].equalsIgnoreCase("pay")) {
 					Player r = Bukkit.getPlayer(args[1]);
-					if(r != null){
-						if (Pattern.matches("[a-zA-Z]+", args[2]) == false) {							
-							coinsAPI.removeCoins(r.getUniqueId(), Integer.parseInt(args[2]));
-						}
-					}
-				} else if(args[0].equalsIgnoreCase("set")){
-					Player r = Bukkit.getPlayer(args[1]);
-					if(r != null){
-						if (Pattern.matches("[a-zA-Z]+", args[2]) == false) {							
+					if (r != null) {
+						if (Pattern.matches("[a-zA-Z]+", args[2]) == false) {
 							coinsAPI.setCoins(r.getUniqueId(), Integer.parseInt(args[2]));
+							player.sendMessage(prefix + "§8§l§c§l- §6§l" + args[2] + "§8§l]");
+							r.sendMessage(prefix + "§8§l§a§l+ §6§l" + args[2] + "§8§l]");
 						}
 					}
 				}
@@ -67,5 +86,5 @@ public class Coins extends JavaPlugin {
 		}
 		return false;
 	}
-	
+
 }
